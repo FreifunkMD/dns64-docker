281b253c8d278fca6b102461be39c28771646885
Jasper Orschulko <jasper@fancydomain.eu>
2018-11-03 01:09:51 +0100

    initial commit

---
+++ build/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cat > /etc/bind/named.conf.options <<EOF
+options {
+        directory "/var/cache/bind";
+        auth-nxdomain no;
+	listen-on { ${DNS64_IP6_LISTEN}; };
+        listen-on-v6 { ${DNS64_IP6_LISTEN}; };
+        allow-query { any; };
+        dns64 ${DNS64_STATIC_PREFIX} {
+                clients { any; };
+        };
+};
+EOF
+
+cat /etc/bind/named.conf.options
+if [ "$1" = 'named' ]; then
+  echo "Starting named..."
+  exec $(which named) -g
+else
+  exec "$@"
+fi
